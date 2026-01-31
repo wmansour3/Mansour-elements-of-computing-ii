8145e5aebd20adcbe958f79449242b3ef2578b89
wmansour3 <wmansour@nd.edu>
2026-01-30 21:06:00 -0500

    archer.py updates

--- Week 3/game_project/characters/archer.py
+++ Week 3/game_project/characters/archer.py
@@ -1,13 +1,34 @@
 from .warrior import Warrior
 
 class Archer(Warrior):
+    """
+    Represents an Archer character, inheriting from Warrior.
+
+    Attributes:
+        name (str): The archer's name.
+        level (int): The archer's level.
+        health (int): The archer's current health.
+        strength (int): The archer's strength.
+        arrows (int): The archer's amount of arrows left.
+    """
     def __init__(self, name, level, health, strength, arrows):
-        """Initializes a new Archer object."""
+        """Initializes a new Archer object.
+        """
         super().__init__(name, level, health, strength)
 
         self.arrows = arrows
 
     def fire_arrow(self, target):
+        """
+        Fires an arrow at the target, reducing their health by the archer's strength.
+
+        Args:
+            target (Character): The target of the arrow attack.
+
+        Returns:
+            str: A message describing the arrow attack.
+        """
+        
         if self.arrows > 0:
             self.arrows -= 1
             damage = self.strength
